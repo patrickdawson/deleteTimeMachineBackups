f6fe4f7b0dfaf3ae58226c3e71b3035a861df8d7
Patrick Dawson <patrick.dawson79@gmail.com>
2021-12-25 17:02:17 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # deleteTimeMachineBackups
+
+Small node application that shows your existing time machine backups and presents them with an inquirer form. You can then check backups you want to delete.
+
+**NOTE** In order to delete time machine backups this script needs to be run with admin priviledges.
+
+## Installation
+
+Checkout this repository.
+Then run:
+
+```
+npm install
+```
+
+## Usage
+
+Show help
+
+```
+node app.js -h
+```
+
+Dry Run (don't delete anything, only print delete commands)
+
+```
+node app.js -h
+```
+
+Default run that actually deletes selected backups
+
+```
+node app.js
+```
---
+++ app.js
@@ -0,0 +1,66 @@
+const process = require('process');
+const sh = require('shelljs');
+const inquirer = require('inquirer');
+const { program } = require('commander');
+const { version } = require('./package.json');
+
+async function getExistingBackups() {
+  const { stdout } = sh.exec('tmutil listbackups', { silent: true });
+  const backups = stdout
+    .split('\n')
+    .filter((e) => e !== '')
+    .map((e) => e.replace('.backup', ''));
+
+  const answers = await inquirer.prompt([
+    {
+      name: 'backupsToDelete',
+      type: 'checkbox',
+      message: 'Select backups to delete',
+      choices: backups,
+    },
+  ]);
+
+  return answers.backupsToDelete;
+}
+
+async function getBackupMountPoint() {
+  const cmd = 'tmutil destinationinfo';
+  const { stdout } = sh.exec(cmd, { silent: true });
+  const destInfo = stdout
+    .split('\n')
+    .filter((e) => e.startsWith('Mount Point'));
+
+  if (destInfo.length !== 1) {
+    throw new Error(`Mount Point not found with cmd: ${cmd}`);
+  }
+  const mountPointInfo = destInfo[0].split(':');
+  if (mountPointInfo.length !== 2) {
+    throw new Error(`Failed to extract mount point from: ${mountPointInfo}`);
+  }
+  return mountPointInfo[1].trim();
+}
+
+async function main() {
+  program.version(version);
+  program.option(
+    '-d, --dryRun',
+    "Don't delete anything. Only output delete commands"
+  );
+
+  program.parse(process.argv);
+
+  const backups = await getExistingBackups();
+  const backupMountPoint = await getBackupMountPoint();
+
+  for (const backup of backups) {
+    const cmd = `sudo tmutil delete -d '${backupMountPoint}' -t ${backup}`;
+    if (program.opts().dryRun) {
+      console.log(cmd);
+    } else {
+      console.log(`Deleting backup: ${backup}`);
+      sh.exec(cmd);
+    }
+  }
+}
+
+main().catch((e) => console.error(e));
